ee93dc0cf028c4e2e741ec3019c034c9d5b2d132
dreambuffer8192 <dreambuffer8192@gmail.com>
2024-09-23 18:58:12 -0300

    Update fragment.js

--- fragment.js
+++ fragment.js
@@ -1,9 +1,8 @@
 class CustomComponent extends HTMLElement {
 	constructor() {
 		super();
+		this.innerHTML = "<p>No veo peliculas :(</p>";
 	}
-	
-	this.innerHTML = "<p>No veo peliculas :(</p>";
 }
 customElements.define("team-fmb", CustomComponent);
 
